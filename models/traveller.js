0d257d9396a37109ecd5813179ffda46dfaffef5
Michael Wales <michaelwales@gmail.com>
2020-04-15 19:12:32 +0100

    Adds Code for the first two functions

--- models/traveller.js
+++ models/traveller.js
@@ -3,11 +3,17 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const journeyStartLocations = this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+  return journeyStartLocations;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const journeyEndLocations = this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
+  return journeyEndLocations;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
